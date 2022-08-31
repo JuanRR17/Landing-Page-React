de1574ef66d7aafe4e4e984832a410ed76f9913c
JuanRR17 <juanrepeto@gmail.com>
2022-08-31 17:16:21 +0200

    finished

--- src/js/component/CardsContainer.jsx
+++ src/js/component/CardsContainer.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import Card from "./Card.jsx";
 
 const CardsContainer = () =>{
-	const cards = Array.of(4).map(card => {
-		<Card />
-});
 
 	return (
 	<div className="row mt-3 g-3">
-		<Card />
-		<Card />
-		<Card />
-		<Card />
+		{Array(4).fill("").map((_,idx) => {
+          return <Card key={idx} />
+        })}
 	</div>
 	);
 }
--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import MenuItem from "./MenuItem.jsx";
+
 
 const NavBar = ()=>{
+
+  const names = ["Home", "About", "Services", "Contact"];
+
     return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-      <div class="container-fluid justify-content-around">
+      <div className="container-fluid justify-content-around">
         <a className="navbar-brand mx-5" href="#">Start Bootstrap</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -11,18 +16,10 @@ const NavBar = ()=>{
 
         <div className="collapse navbar-collapse justify-content-end mx-5" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Services <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact <span className="sr-only">(current)</span></a>
-            </li>
+            <MenuItem name={names[0]} active={true} />
+            <MenuItem name={names[1]} />
+            <MenuItem name={names[2]} />
+            <MenuItem name={names[3]} />
           </ul>
         </div>
       </div>
